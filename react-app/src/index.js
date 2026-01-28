7e235be8efc456d3a00974b50adddbfb1f72698b
tomerosh <tomerosh@gmail.com>
2026-01-28 11:00:48 +0200

    gh-pages fix

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/little-lemon-front-end-project-' >
+    <BrowserRouter basename='/Little-Lemon' >
       <App />
     </BrowserRouter>
   </React.StrictMode>
