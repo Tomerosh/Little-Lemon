63c0dd54bd48f6e97a6d0a3cac4afde5801efe55
Tomerosh <tomerosh@gmail.com>
2026-06-01 15:47:45 +0300

    BookingPage label

--- react-app/src/components/BookingPage.js
+++ react-app/src/components/BookingPage.js
@@ -4,6 +4,7 @@ import BookingForm from "./BookingForm";
 export default function BookingPage({availableTimes, changeTimes}) {
     
     return <>
+        <label className="booking-label">RESERVE A TABLE</label>
     <BookingForm availableTimes={availableTimes} changeTimes={changeTimes}/>
     </>
 }
